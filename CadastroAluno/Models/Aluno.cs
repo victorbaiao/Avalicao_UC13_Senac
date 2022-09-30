b89a26569e1c1618c64b44359b8fa52465f15cea
AecioSenac <aeciobrito@gmail.com>
2022-09-29 21:47:23 -0300

    Criacao da modelo do aluno

---
+++ CadastroAluno/Models/Aluno.cs
@@ -0,0 +1,24 @@
+﻿namespace CadastroAluno.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Turma { get; set; }
+        public double Media { get; set; }
+
+        public void AtualizarDados(string nome, string turma)
+        {
+            Nome = nome;
+            Turma = turma;
+        }
+
+        public bool VerificaAprovacao() 
+            => Media > 5;
+
+        public void AtualizaMedia(double novaMedia)
+        {
+            Media = novaMedia;
+        }
+    }
+}
